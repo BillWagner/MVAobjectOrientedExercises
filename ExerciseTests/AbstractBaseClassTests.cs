dfa3a41ec83394cf8436beede37557e55e182b8d
Bill Wagner <bill@thebillwagner.com>
2015-11-06 14:04:23 -0800

    Make the Person class abstract.

---
+++ ExerciseTests/AbstractBaseClassTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseTests
+{
+    class AbstractBaseClassTests
+    {
+
+        //  In this section, we’ll revisit our current class hierarchy 
+        // and determine if any of our classes should be abstract:
+        // Are there types that are never instantiated?
+        // Is that by design, or simply based on the features we’ve created so far?
+        // Should any of the abstract types have abstract members?
+
+        // So, there aren't any new features that warrant new tests. 
+        // That's why this is an empty test file.
+    }
+}
